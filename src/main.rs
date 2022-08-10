8053739b3e6f2dba6aa5786a27215391bcd751e6
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2022-08-10 17:09:11 +1000

    some cleaning

--- src/main.rs
+++ src/main.rs
@@ -228,18 +228,18 @@ fn letter_key(letter: char) -> impl Widget<WobbleState> {
 		.on_click(move |_ctx, data: &mut WobbleState, _| {
 			// call function to analyze data.guesses and return (guess_word,guess_letter) coordinates
 			let (guess_word, guess_letter) = get_current_position(&data.guesses);
-			data.guesses[guess_word][guess_letter].0 = letter;
 			let next_guess_letter = if guess_letter == 4 {
 				4
 			} else {
 				guess_letter + 1
 			};
+			data.guesses[guess_word][guess_letter].0 = letter;
 			data.guesses[guess_word][guess_letter].1 = LetterState::Set;
 			data.guesses[guess_word][next_guess_letter].1 = LetterState::Input;
 		})
 }
 
-fn button(label: String) -> impl Widget<WobbleState> {
+fn enter_key(label: String) -> impl Widget<WobbleState> {
 	let rect = Painter::new(|ctx, _, _| {
 		let bounds = ctx.size().to_rect().to_rounded_rect(2.0);
 
@@ -507,7 +507,7 @@ fn build_layout() -> impl Widget<WobbleState> {
 		.with_spacer(1.0)
 		.with_flex_child(
 			Flex::row()
-				.with_flex_child(button("Enter".to_string()), 1.0)
+				.with_flex_child(enter_key("Enter".to_string()), 1.0)
 				.with_spacer(1.0)
 				.with_flex_child(letter_key('z'), key_space)
 				.with_spacer(key_space)
